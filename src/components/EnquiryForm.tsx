ac5b7a1dd9ef6f6d4b40b94f160468da780d4cdb
Mihail Marian <mihailmarian12@gmail.com>
2023-04-11 15:51:01 +0100

    wip

--- src/components/EnquiryForm.tsx
+++ src/components/EnquiryForm.tsx
@@ -1,3 +1,22 @@
+import { TextField, Button } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
+
 export default function EnquiryForm() {
-  return <div>hello world</div>;
+  return (
+    <Grid component="form" container spacing={2}>
+      <Grid md={8} xs={12}>
+        <TextField
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+        />
+      </Grid>
+      <Grid md={4} xs={12}>
+        <Button variant="contained">Submit</Button>
+      </Grid>
+    </Grid>
+  );
 }
